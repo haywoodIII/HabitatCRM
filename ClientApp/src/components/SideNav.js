36e094bb3bc14ff88a599f5d28cdfb09e643abeb
Haywood <haywoodphillips@gmail.com>
2020-10-22 14:15:04 -0400

    Add basic donor submission form

--- ClientApp/src/components/SideNav.js
+++ ClientApp/src/components/SideNav.js
@@ -2,8 +2,9 @@
 import { Layout, Menu } from 'antd';
 import { DesktopOutlined, PieChartOutlined } from '@ant-design/icons';
 import { Link } from "react-router-dom"; 
+import './SideNav.css';
 
-const { Header, Content, Footer, Sider } = Layout;
+const { Header, Content, Sider } = Layout;
 
 
 export class SideNav extends React.Component {
@@ -19,28 +20,29 @@ export class SideNav extends React.Component {
     render() {
         const { collapsed } = this.state;
         return (
-            <Layout style={{ minHeight: '100vh' }}>
-                <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
+            <Layout className="site-layout-background" style={{ minHeight: '100vh', margin: "none" }}>
+                <Header className="header">
                     <div className="logo" />
-                    <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                        <Menu.Item key="1" icon={<PieChartOutlined />}>
-                            <Link to="/">Home</Link>
-                        </Menu.Item>
-                        <Menu.Item key="2" icon={<DesktopOutlined />}> 
-                            <Link to="/counter">Counter</Link>
-                        </Menu.Item>
-                    </Menu>
-                </Sider>
-                <Layout className="site-layout">
-                    <Header className="site-layout-background" style={{ padding: 0 }} />
-                    <Content style={{ margin: '0 16px' }}>
+                </Header>
+                <Layout>
+                    <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
+                        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                            <Menu.Item key="1" icon={<PieChartOutlined />}>
+                                <Link to="/">Home</Link>
+                            </Menu.Item>
+                            <Menu.Item key="2" icon={<DesktopOutlined />}> 
+                                <Link to="/donors">Donors</Link>
+                            </Menu.Item>
+                        </Menu>
+                    </Sider>
+                    <Content className="site-layout-background">
                         <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
                             {this.props.children}
                         </div>
                     </Content>
-                    <Footer style={{ textAlign: 'center' }}>Created by Habitat For Humanity of Wayne County</Footer>
-                </Layout>
+                </Layout> 
             </Layout>
         );
     }
 }
+
---
+++ ClientApp/src/components/donors/DonorsService.js
@@ -0,0 +1,15 @@
+﻿import React, { useState } from 'react';
+import { Row, Col } from 'antd';
+
+
+
+function Donors {
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+      </button>
+        </div>
+    );
+}
